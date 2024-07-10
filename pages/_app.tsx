bf328c5ab0e13a776cca796b9a89cb09151a8624
Dumitru Mihai <jurja.dumitru@gmail.com>
2024-07-10 12:53:25 +0300

    Modify _app and tailwind configuration so the fonts are available throughout the website

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { dmSans, dmSerifDisplay } from '@/styles/fonts/fonts';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${dmSans.variable} ${dmSerifDisplay.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
